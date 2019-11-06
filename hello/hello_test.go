8c79a5908b2bb3f1c55db10d117ec20fcb3d5dcf
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-05 20:39:19 -0600

    Another hello refactor

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,14 +12,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Say hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("Say 'hello, world' when an empty string is provided", func(t *testing.T) {
 
-		got := Hello("")
+	t.Run("Say 'hello, world' when an empty string is provided", func(t *testing.T) {
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 
