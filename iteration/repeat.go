66e07a31d1262be23e18e386c04b741db168a6f9
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-06 20:39:15 -0600

    Add comment

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,6 +1,7 @@
 // Package iteration provides ...
 package iteration
 
+// Repeat repeats a string.
 func Repeat(input string, count int) string {
 	var repeated string
 	for index := 0; index < count; index++ {
