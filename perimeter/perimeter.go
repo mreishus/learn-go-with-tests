e0c400f580690caf8878954b8fddab4f17fc4c21
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-13 02:49:32 +0000

    Finish up perimeter

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -11,9 +11,12 @@ type Rectangle struct {
 	Width  float64
 	Height float64
 }
-
 type Circle struct {
 	Radius float64
+}
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
 func (r Rectangle) Area() float64 {
@@ -21,6 +24,9 @@ func (r Rectangle) Area() float64 {
 }
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+func (t Triangle) Area() float64 {
+	return t.Height * t.Base * 0.5
 }
 
 func Perimeter(rectangle Rectangle) float64 {
