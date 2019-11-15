99d6ec63255a218a9849af51bfba4a32513a4a32
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-15 04:09:56 +0000

    Change to Bitcoin custom type

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,13 +1,24 @@
 // Package main provides ...
 package main
 
+import (
+	"fmt"
+)
+
+type Bitcoin int
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
-func (w Wallet) Balance() int {
+func (w Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+// Implement "Stringer", defined in fmt,
+// defines how type is printed in %s format
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
