2839b20b68c3165199e8f6c2d7c7095e7f10916f
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-15 04:09:30 +0000

    Simple wallet

---
+++ wallet/wallet.go
@@ -0,0 +1,13 @@
+// Package main provides ...
+package main
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+func (w Wallet) Balance() int {
+	return w.balance
+}
