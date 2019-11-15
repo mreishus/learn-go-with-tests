e13d87b56a5cf18e1f136fe2e44a405208e0d1f5
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-15 04:37:28 +0000

    Add errors to wallet

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -12,6 +13,13 @@ type Wallet struct {
 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
+	w.balance -= amount
+	return nil
 }
 func (w Wallet) Balance() Bitcoin {
 	return w.balance
