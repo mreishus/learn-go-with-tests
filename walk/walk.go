279bca70995438305e7c6d56d4fe9af6e79bef0e
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-25 16:54:16 -0600

    walk: unsafe and naive version

---
+++ walk/walk.go
@@ -0,0 +1,10 @@
+// Package main provides ...
+package main
+
+import "reflect"
+
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
+}
