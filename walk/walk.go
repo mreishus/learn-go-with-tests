c9c1bcf509be8cee5f748bf8c07aa98ce295aa72
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-25 17:11:29 -0600

    walk: handle pointers

--- walk/walk.go
+++ walk/walk.go
@@ -6,12 +6,16 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		if field.Kind() == reflect.String {
+		switch field.Kind() {
+		case reflect.String:
 			fn(field.String())
-		}
-		if field.Kind() == reflect.Struct {
+		case reflect.Struct:
 			walk(field.Interface(), fn)
 		}
 	}
