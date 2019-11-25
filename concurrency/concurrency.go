09b44f052bac3afdba12924b39950dee4aec97ce
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-25 10:56:53 -0600

    concurrency: Implement properly

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -3,18 +3,27 @@ package concurrency
 
 // go test -race
 
-import "time"
-
 type WebSiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebSiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultsChannel := make(chan result)
+
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			// results[u] = wc(u)
+			resultsChannel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		result := <-resultsChannel
+		results[result.string] = result.bool
+	}
+
 	return results
 }
