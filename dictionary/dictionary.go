a134c8e8d4a76521136bab91531f3e002dd9009f
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-25 03:47:50 +0000

    Refactor dictionary: Make a type and method

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,6 +1,11 @@
 // Package main provides ...
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+type Dictionary map[string]string
+
+// func Search(dictionary map[string]string, word string) string {
+// 	return dictionary[word]
+// }
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -4,9 +4,9 @@ package main
 import "testing"
 
 func TestSearc(t *testing.T) {
-	dictionary := map[string]string{"test": "just a test"}
+	dictionary := Dictionary{"test": "just a test"}
 
-	got := Search(dictionary, "test")
+	got := dictionary.Search("test")
 	want := "just a test"
 
 	assertStrings(t, got, want)
