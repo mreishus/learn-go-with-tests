4ee6ad70194e2a33873d091373d4c7781a2657c9
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-25 03:49:43 +0000

    Refactor dictionary: Make an error case on no lookup

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -6,10 +6,22 @@ import "testing"
 func TestSearc(t *testing.T) {
 	dictionary := Dictionary{"test": "just a test"}
 
-	got := dictionary.Search("test")
-	want := "just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("Expected to get an error")
+		}
+		got := err.Error()
+		assertStrings(t, got, want)
+	})
 }
 
 func assertStrings(t *testing.T, got string, want string) {
