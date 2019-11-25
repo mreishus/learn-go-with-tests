2bf1d52f57b90d46093c9c1e47eb8afa35501fb8
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-25 10:53:33 -0600

    concurrency: Add concurrency, but incorrectly
    Sometimes crashes due to multiple functions writing to the
    map at the same time.
    Also, sleeps for 2 hardcoded seconds to wait for results which
    is janky.

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,12 +1,20 @@
 // Package concurrency provides ...
 package concurrency
 
+// go test -race
+
+import "time"
+
 type WebSiteChecker func(string) bool
 
 func CheckWebsites(wc WebSiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			results[u] = wc(u)
+		}(url)
 	}
+
+	time.Sleep(2 * time.Second)
 	return results
 }
