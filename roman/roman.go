30d50670de6d1a6a2de77b15490facc521561de2
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-26 01:50:54 +0000

    roman: dumb implementation

---
+++ roman/roman.go
@@ -0,0 +1,14 @@
+// Package main provides ...
+package main
+
+import (
+	"strings"
+)
+
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+	for i := 0; i < arabic; i++ {
+		result.WriteString("I")
+	}
+	return result.String()
+}
