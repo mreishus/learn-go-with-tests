681446af40d2a0c23546c55c0b22d0cdc303e2b9
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-26 01:54:14 +0000

    roman: handle 9/10

--- roman/roman.go
+++ roman/roman.go
@@ -9,6 +9,12 @@ func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 	for arabic > 0 {
 		switch {
+		case arabic > 9:
+			result.WriteString("X")
+			arabic -= 10
+		case arabic > 8:
+			result.WriteString("IX")
+			arabic -= 9
 		case arabic > 4:
 			result.WriteString("V")
 			arabic -= 5
