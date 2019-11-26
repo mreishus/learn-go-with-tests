aa9827544b529493a2cb852968920a92b045e40c
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-26 03:40:10 +0000

    server: add 404 behavior and test

--- server/server_test.go
+++ server/server_test.go
@@ -26,16 +26,23 @@ func TestGETPlayers(t *testing.T) {
 	}
 	server := &PlayerServer{&store}
 
+	t.Run("return 404 on missing players", func(t *testing.T) {
+		request := newGetScoreRequest("nothere")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusNotFound)
+	})
+
 	t.Run("returns Pepper's score", func(t *testing.T) {
 		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
-		got := response.Body.String()
-		want := "20"
-
-		assertResponseBody(t, got, want)
+		assertResponseBody(t, response.Body.String(), "20")
+		assertStatus(t, response.Code, http.StatusOK)
 	})
 	t.Run("returns Floyd's score", func(t *testing.T) {
 		request := newGetScoreRequest("Floyd")
@@ -43,10 +50,8 @@ func TestGETPlayers(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
-		got := response.Body.String()
-		want := "10"
-
-		assertResponseBody(t, got, want)
+		assertResponseBody(t, response.Body.String(), "10")
+		assertStatus(t, response.Code, http.StatusOK)
 	})
 }
 
@@ -54,6 +59,13 @@ func newGetScoreRequest(name string) *http.Request {
 	url := fmt.Sprintf("/players/%s", name)
 	request, _ := http.NewRequest(http.MethodGet, url, nil)
 	return request
+}
+
+func assertStatus(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("status wrong, got %q want %q", got, want)
+	}
 }
 
 func assertResponseBody(t *testing.T, got, want string) {
