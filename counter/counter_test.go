73ba197e2095de3f5feac2ce2d8d3e475123299c
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-26 00:41:28 +0000

    counter: failing concurrent test

--- counter/counter_test.go
+++ counter/counter_test.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -13,11 +14,29 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		assertCounter(t, counter, 3)
 	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
 }
 
 func assertCounter(t *testing.T, got Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
-		t.Errorf("got %d want %d", got.Value(), 3)
+		t.Errorf("got %d want %d", got.Value(), want)
 	}
 }
