deeda9ce12f3a79b883795b0ac881b1655ef1f1e
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-26 01:06:29 +0000

    context: happy path

---
+++ context/server.go
@@ -0,0 +1,17 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
