a29f9de3a5e700a03baad0d3166ef0890e005f21
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-26 03:11:18 +0000

    server: start project

---
+++ server/server.go
@@ -0,0 +1,30 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
+
+}
+
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
